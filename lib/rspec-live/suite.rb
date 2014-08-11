57835994e1dadfeeb3754e4cf77687dc2eb89fca
Brian Auton <brianauton@gmail.com>
2014-08-11 17:17:23 -0400

    Simplify example storage in suite and keep names sorted

--- lib/rspec-live/suite.rb
+++ lib/rspec-live/suite.rb
@@ -5,30 +5,31 @@ module RSpecLive
     def initialize(runner, display)
       @runner = runner
       @display = display
+      @example_names = []
       @examples = {}
     end
 
     def inventory
-      new_examples = {}
+      @example_names = []
       @runner.inventory do |example_data|
-        name = example_data["name"]
-        new_examples[name] = find_or_create_example(name, example_data)
+        find_or_create_example example_data
       end
-      @examples = new_examples
     end
 
     def update
       @runner.update do |example_data|
-        name = example_data["name"]
-        example = find_or_create_example(name, example_data)
-        example.status = example_data["status"]
+        find_or_create_example(example_data).status = example_data["status"]
       end
     end
 
     private
 
-    def find_or_create_example(name, data)
-      @examples[name] || Example.new(data, @display.example_display)
+    def find_or_create_example(data)
+      name = data["name"]
+      @example_names << name unless @example_names.include? name
+      @example_names.sort!
+      @examples[name] ||= Example.new(data, @display.example_display)
+      @examples[name]
     end
   end
 end

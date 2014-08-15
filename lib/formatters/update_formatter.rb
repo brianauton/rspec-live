5c5b0f5854563ffec671e3e76e84dc81f3a92cde
Brian Auton <brianauton@gmail.com>
2014-08-15 19:36:41 -0400

    [#2] Report coverage by file when running specs

--- lib/formatters/update_formatter.rb
+++ lib/formatters/update_formatter.rb
@@ -2,29 +2,47 @@ require "json"
 
 class UpdateFormatter
   RSpec::Core::Formatters.register self, :example_passed, :example_failed, :example_pending
+  RSpec.configure do |c|
+    c.around(:example) do |example|
+      example.metadata[:files_touched] = {}
+      set_trace_func proc { |event, file| example.metadata[:files_touched][file] = true }
+      example.run
+      set_trace_func nil
+    end
+  end
 
   def initialize(output)
     @output = output
   end
 
   def example_passed(notification)
-    report :name => notification.example.location, :status => "passed"
+    report notification, "passed"
   end
 
   def example_failed(notification)
-    report({
-      :name => notification.example.location,
-      :status => "failed",
+    report notification, "failed", {
       :backtrace => notification.exception.backtrace,
       :message => notification.exception.message,
-    })
+    }
   end
 
   def example_pending(notification)
-    report :name => notification.example.location, :status => "pending"
+    report notification, "pending"
   end
 
-  def report(data)
-    @output << "#{JSON.unparse data}\n"
+  private
+
+  def report(notification, status, options = {})
+    data = {
+      :name => notification.example.location,
+      :status => status,
+      :files_touched => filtered_files_touched(notification),
+    }
+    @output << "#{JSON.unparse data.merge(options)}\n"
+  end
+
+  def filtered_files_touched(notification)
+    raw_files = notification.example.metadata[:files_touched].keys
+    raw_files.select { |file| file.start_with? Dir.pwd }
   end
 end
